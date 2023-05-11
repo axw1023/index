4f39747e73931a2f80ad1f26ae4888d99dde0513
刘金星 <395590257@qq.com>
2023-05-11 17:03:35 +0800

    修复前端获取雪花id长度问题精度缺失问题

--- link/src/main/java/com/redemption/link/vo/Converter.java
+++ link/src/main/java/com/redemption/link/vo/Converter.java
@@ -1,9 +1,10 @@
 package com.redemption.link.vo;
 
 
+import com.redemption.link.entity.Link;
 import com.redemption.link.entity.Subject;
-import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,7 +12,10 @@ public abstract class Converter {
 
     public static Converter INSTANT = Mappers.getMapper(Converter.class);
 
-    public abstract SubjectVo convert(Subject subject);
+    //前端获取雪花id长度问题精度缺失，转为String类型
+    @Mapping(source = "id", target = "id", resultType = String.class)
+    public abstract SubjectVo convertSubjectVo(Subject subject);
 
-    public abstract List<SubjectVo> covertList(List<Subject> subjectList);
+    public abstract LinkVo convertLinkVo(Link link);
+
 }
